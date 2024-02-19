512f691165c727ddd1098cd00d964662d648153d
Ramonak321 <thvolk13@gmail.com>
2024-02-19 17:32:34 +0300

    4

--- kz.py
+++ kz.py
@@ -1,18 +1,30 @@
 # Задача: Написать программу, которая из имеющегося массива строк 
 # формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
-# Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
-# При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+# Первоначальный массив можно ввести с клавиатуры, либо задать на старте 
+# выполнения алгоритма. 
+# При решении не рекомендуется пользоваться коллекциями, 
+# лучше обойтись исключительно массивами.
 
+def add_array(n):
+    i = 0
+    while i < n:
+        txt = input()
+        if txt != 'q':
+            txt_arr[i] = txt
+        else:
+            return txt_arr
+        i += 1
+    return txt_arr
+
+def search_three(txt_arr):
+    for i in txt_arr:
+        if len(txt_arr[i]) == 3:
+             txt_arr[i]
+        i += 1
+
+print("pls insert value:")
+n = input()
 print("Insert text or q:")
 txt_arr = []
-txt = 1
-i = 0
-while txt != "q":
-    txt = input()
-    txt_arr[i] = txt
-    i += 1
-
-for element in txt_arr:
-    if len(txt_arr[i]) > 3:
-        print(txt_arr[i])
-    i += 1
+txt_arr = add_array(n)
+print(search_three())
