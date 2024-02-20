940cc79775348b0bd9b28d87b62a2c31f7af92c3
Ramonak321 <thvolk13@gmail.com>
2024-02-20 08:54:21 +0300

    final1

--- kz.py
+++ kz.py
@@ -5,26 +5,26 @@
 # При решении не рекомендуется пользоваться коллекциями, 
 # лучше обойтись исключительно массивами.
 
-def add_array(n):
+def add_array(txt_arr):
     i = 0
     while i < n:
         txt = input()
         if txt != 'q':
-            txt_arr[i] = txt
+            txt_arr.append(txt)
         else:
             return txt_arr
         i += 1
     return txt_arr
 
 def search_three(txt_arr):
-    for i in txt_arr:
-        if len(txt_arr[i]) == 3:
-             txt_arr[i]
-        i += 1
+    for element in txt_arr:
+        if len(element) == 3:
+            print(element)
+    return txt_arr
 
-print("pls insert value:")
-n = input()
-print("Insert text or q:")
+print("pls insert value array:")
+n = int(input())
+print("Insert text or 'q' for end:")
 txt_arr = []
-txt_arr = add_array(n)
-print(search_three())
+txt_arr = add_array(txt_arr)
+search_three(txt_arr)
